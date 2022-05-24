4288fe7c9da785743339f3d65c73b1d48374dd9a
Jaxon Adams <jadamsresume1@gmail.com>
2022-05-24 17:27:28 -0600

    homepage components set up

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import IconList from './IconList';
 
 const Home = () => {
     return (
         <div id='home'>
             <h1 className='lg-heading'>Jaxon <span className='secondary-text'>Adams</span></h1>
             <h2 className='sm-heading'>Full Stack Developer</h2>
+            <IconList />
         </div>
     );
 };
--- src/components/IconList.js
+++ src/components/IconList.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Github, Instagram, Linkedin } from 'react-bootstrap-icons';
 
 const IconList = () => {
     return (
         <div id='icons'>
-            <a href='https://github.com/JaxonAdams' target='_blank' className='nav-link'>
-                
+            <a href='https://github.com/JaxonAdams' rel='noreferrer' target='_blank' className='nav-link'>
+                <Github className='icon-link' />
+            </a>
+            <a href='https://www.instagram.com/jj_adams01/' rel='noreferrer' target='_blank' className='nav-link'>
+                <Instagram className='icon-link' />
+            </a>
+            <a href='https://www.linkedin.com/in/jaxon-adams-ba5743229' rel='noreferrer' target='_blank' className='nav-link'>
+                <Linkedin className='icon-link' />
             </a>
         </div>
     );
